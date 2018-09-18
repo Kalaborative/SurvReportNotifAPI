08347ccc6004d7f4466d2c0fc6d5c9684fe6572b
Drew Davis <kalaborative94@gmail.com>
2018-09-18 14:08:49 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+import requests
+import json
+from os import environ
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return "Hello."
+
+simpleData = {
+	"status": "OK",
+	"requested_at": datetime.now()
+}
+
+api = {'api': 'key-8e60a5b2a29f6d59869db87cb87caee1'}
+
+emailData = {
+	"from": "Zenmark Mail <postmaster@vizionary-dev.xyz>",
+	"to": "kalaborative94@gmail.com",
+	"subject": "New Survivio+ JS Error Report!",
+}
+
+@app.route("/api/report", methods=["GET", "POST"])
+def report():
+	if request.method == "GET":
+		errorData = request.args.get('body')
+		if errorData:
+			return jsonify({"status": "OK", "errors": errorData})
+		else:
+			return jsonify(simpleData)
+	if request.method == "POST":
+		errorData = request.form
+		errorData = errorData.to_dict()
+		# print(errorData)
+		emailData["text"] = "New message! \n " + json.dumps(errorData, indent=4)
+		r = requests.post('https://api.mailgun.net/v3/vizionary-dev.xyz/messages', auth=('api', api['api']), data=emailData)
+		# print(r.status_code)
+		return jsonify({"status": "OK"})
+
+
+if __name__ == "__main__":
+	# convention to run on Heroku
+	port = int(environ.get("PORT", 5000))
+	# run the app available anywhere on the network, on debug mode
+	app.run(host="0.0.0.0", port=port, debug=True)
